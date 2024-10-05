ddb6cf9f88b0df3beab1ffdc16d750ae6a855a1f
Anisa Abdullahi <abdullahianisa9@gmail.com>
2024-10-05 19:33:04 +0300

    commit again

--- registers.php
+++ registers.php
@@ -7,30 +7,24 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class=" container mt-5">
+    <div class="container mt-5">
         <h1>Register</h1>
-        <form action="registers.php" method="post">
+        <form action="register.php" method="post"> 
             <div class="mb-3">
-                <label for="username" class="form-label">username</label> 
+                <label for="username" class="form-label">Username</label> 
                 <input type="text" class="form-control" id="username" name="username" required>
-    </div>
-    <div class="mb-3">
-        <label for="username" class="form-label">username</label>
-        <input type="text" class="form-control" id="username" name="username" required>
-    </div>
-    <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-    <div class="mb-3">
-        <label for="password" class="form-label">password</label>
-        <input type="password" class="form-control" id="password" name="password" required>
-
-    </div>
-    <button type="submit" class="btn btn-primary">register</button>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" name="email" placeholder="name@example.com" required> 
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
         </form>
-        <p class="mt-3">Already have an account? <a href="signup.php">Sign in here</a></p>
+        <p class="mt-3">Already have an account? <a href="signin.php">Sign in here</a></p>
     </div>
-   
 </body>
 </html>
